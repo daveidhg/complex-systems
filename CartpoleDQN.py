cd3cf23a91244aa87b87453f206ceef5799ecb15
Martin Risøy <martiris@hiof.no>
2024-03-18 15:10:29 +0100

    Implemented the template for CA
    Co-authored-by: daveidhg <daveidhg@users.noreply.github.com>
    Co-authored-by: Peter Brännström <peterjbrannstrom@gmail.com>

--- CartpoleDQN.py
+++ CartpoleDQN.py
@@ -1,9 +1,10 @@
 import gymnasium as gym
 import time
 
-env = gym.make(id='CartPole-v1')
+env = gym.make(id='MountainCar-v0', render_mode="human")
 (state, _) = env.reset()
-x, velocity, angle, angular_momentum = state
+x, velocity = state
+print(state)
 
 # simulate the environment
 episodeNumber = 200
@@ -14,7 +15,7 @@ timeStepsList = []
 for episodeIndex in range(episodeNumber):
     start = time.time()
     if episodeNumber - episodeIndex == 4:
-        env = gym.make(id='CartPole-v1', render_mode='human')
+        env = gym.make(id='MountainCar-v0', render_mode='human')
         time_a = 0.1
         time_b = 1
     else:
@@ -38,3 +39,32 @@ for episodeIndex in range(episodeNumber):
     stop = time.time()
 
 env.close()
+
+
+import pycxsimulator
+from pylab import *
+
+# import necessary modules
+# define model parameters
+
+def initialize():
+    global env, config # list global variables
+    env = gym.make(id='MountainCar-v0', render_mode="human")
+    (state, _) = env.reset()
+    position, velocity = state
+    config = zeros([1, 100]) # Change to the actual values we are supposed to use
+    
+def observe():
+    global observation, config # list global variables
+    cla() # to clear the visualization space
+    imshow(config, vmin = 0, vmax = 1, cmap = cm.binary)
+    # visualize system states
+
+def update():
+    global env, action, observation, config
+    controller_output = 0
+    observation, reward, terminated, truncated, info = env.step(controller_output)
+    config = append(config, zeros[1, 100]) # Change to the actual values we are supposed to use
+    # update system states for one discrete time step
+
+pycxsimulator.GUI().start(func=[initialize, observe, update])
