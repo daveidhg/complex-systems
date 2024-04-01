e933e1f758348c0b7710c825909b209fb7b25ce3
Martin Risøy <martiris@hiof.no>
2024-04-01 20:05:15 +0200

    Added the lookup update functionality
    Co-authored-by: daveidhg <daveidhg@users.noreply.github.com>

--- MountainCarController.py
+++ MountainCarController.py
@@ -1,24 +1,33 @@
 import pycxsimulator
-import sys
 import gymnasium as gym
 from pylab import *
 
+lookup_string = '10101010101010101010101010101010'
+
+def wrapping_slice(lst, *args):
+    return [lst[i % len(lst)] for i in range(*args)]
+
+def bin_list_to_int(bin_list):
+    return_val = 0
+    for i, num in enumerate(bin_list): 
+        return_val += num * (2 ** (4 - i))
+    return return_val
+
 def scale_number(unscaled, to_min, to_max, from_min, from_max):
     return (to_max - to_min) * (unscaled - from_min) / (from_max - from_min) + to_min
-
-lookup_string = sys.argv[1]
 
 def observation_to_binary_list(observation):
     upper_bound = 127
     lower_bound = 0
     bin_pos = bin(int(round(scale_number(observation[0], lower_bound, upper_bound, -1.2, 0.6), 0)))
     bin_vel = bin(int(round(scale_number(observation[1], lower_bound, upper_bound, -0.07, 0.07), 0)))
-    bin_pos = '0' * (10 - len(bin_pos)) + bin_pos[2:]
-    bin_vel = '0' * (10 - len(bin_vel)) + bin_vel[2:]
+    bin_pos = '0' * (9 - len(bin_pos)) + bin_pos[2:]
+    bin_vel = '0' * (9 - len(bin_vel)) + bin_vel[2:]
     return list(map(int, bin_pos + bin_vel))
 
 def initialize():
-    global env, config # list global variables
+    global env, config, iter_count # list global variables
+    iter_count = 0
     env = gym.make(id='MountainCar-v0', render_mode="human")
     state, _ = env.reset()
     config = [observation_to_binary_list(state)] 
@@ -26,23 +35,31 @@ def initialize():
 def observe():
     global config # list global variables
     cla() # to clear the visualization space
-    imshow(config, vmin = 0, vmax = 2, cmap = cm.binary)
+    imshow(config, vmin = 0, vmax = 1, cmap = cm.binary)
     # visualize system states
 
 def update():
-    global env, config
-    controller_output = int(round(mean(config), 0))
-    observation, reward, terminated, truncated, info = env.step(controller_output)
-    scaled_pos_vel = observation_to_binary_list(observation)
-    print(scaled_pos_vel)
-    # scaled_position = scale_number(observation[0], 0, 2, -1.2, 0.6,)
-    # scaled_velocity = scale_number(observation[1], 0, 2, -0.07, 0.07)
-    #print(observation, [scaled_position, scaled_velocity])
-    config = [scaled_pos_vel] 
+    global env, config, iter_count
+
+    # We take 5 iterations to let the algorithm get initialized before making a move.
+    # To use the observations, we represent it in two 7 bit values for position and velocity.
+    if iter_count % 5 == 0:
+        sample = mean(config[-1][5:9])
+        controller_output = 0 if sample < 0.45 else 2 if sample > 0.55 else 1
+        observation, reward, terminated, truncated, info = env.step(controller_output)
+        config = [observation_to_binary_list(observation)]
+        print(len(config[0]))
+    last_update = config[-1]
+    this_update = []
+
+    # We use the 4 closest neighbors of an observation to determine 
+    # what value from the lookup string we should use to update the current element.
+    for i in range(len(last_update)):
+        this_update.append(int(lookup_string[bin_list_to_int(wrapping_slice(last_update, i - 2, i + 3))]))
+    print(this_update)
+    config.append(this_update)
+    iter_count += 1
     # update system states for one discrete time step
 
-
-pycxsimulator.GUI().start(func=[initialize, observe, update])
-print("I AM NOT HERE")
-pycxsimulator.GUI().runEvent()
-
+if __name__ == "__main__":
+    pycxsimulator.GUI().start(func=[initialize, observe, update])
